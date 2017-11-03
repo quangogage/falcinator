780191e3f7a70e49203e7457ffa5821df1dbb1c2
Gage Henderson <gage@quangoinc.com>
2017-11-03 16:13:18 -0700

    --allow-empty-message

--- build/components/Powerups/PowerupHandlers/FastShooting.js
+++ build/components/Powerups/PowerupHandlers/FastShooting.js
@@ -16,5 +16,6 @@ var _jquery2 = _interopRequireDefault(_jquery);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function FastShooting(dt) {
+  console.log('uhh trying to shoot a bullet');
   (0, _Bullet.shootBullet)(_Game.mouseX, _Game.mouseY, _Game.ship, (0, _jquery2.default)('.Game'));
 }
