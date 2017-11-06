4f0f715071fb54fd961dfcec314e2efadead07a0
Gage Henderson <gage@quangoinc.com>
2017-11-06 14:30:19 -0800

    --allow-empty-message

--- build/components/Powerups/HandlePowerups.js
+++ build/components/Powerups/HandlePowerups.js
@@ -44,8 +44,8 @@ function HandlePowerups(dt) {
 // Activate / return new powerup
 function activatePowerup() {
   var activePowerup = powerupList[Math.floor(getRandom(0, powerupList.length - 1))];
-
-  activatePowerup.timer = 0;
+  console.log('powerup activated!');
+  activePowerup.timer = 0;
   activePowerup.active = true;
 }
 
