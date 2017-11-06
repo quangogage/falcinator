2166ecd183f0c51f705ca52e205e0d6ab267df52
Gage Henderson <gage@quangoinc.com>
2017-11-06 15:26:59 -0800

    --allow-empty-message

--- build/components/Powerups/PowerupHandlers/Seeking.js
+++ build/components/Powerups/PowerupHandlers/Seeking.js
@@ -7,4 +7,8 @@ exports.MailSeek = MailSeek;
 
 var _Bullet = require('../../Bullet');
 
-function MailSeek(dt) {}
+function MailSeek(dt) {
+  for (var i = 0; i < _Bullet.bullets.length; i++) {
+    _Bullet.bullets[i].angle += 0.05 * dt;
+  }
+}
