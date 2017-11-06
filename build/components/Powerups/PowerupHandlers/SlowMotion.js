9a44c6df50fc4bb2c629c31c863a765c7389847c
Gage Henderson <gage@quangoinc.com>
2017-11-06 14:19:19 -0800

    --allow-empty-message

--- build/components/Powerups/PowerupHandlers/SlowMotion.js
+++ build/components/Powerups/PowerupHandlers/SlowMotion.js
@@ -1,20 +1,17 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.SlowMotion = SlowMotion;
 exports.handleSlowMo = handleSlowMo;
-var isActive = false;
-function SlowMotion(dt, currentPowerup) {
-  if (currentPowerup === 1) {
-    isActive = true;
-  } else {
-    isActive = false;
-  }
-}
+
+var _HandlePowerups = require('../HandlePowerups');
+
+function SlowMotion(dt, isActiveArg) {}
 
 function handleSlowMo(dt) {
+  var isActive = _HandlePowerups.powerupList[1].active;
   if (isActive === true) {
     return dt / 2;
   } else {
