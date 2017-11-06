0da27c60bb343a424ea9e950d29c3d797600c97c
Gage Henderson <gage@quangoinc.com>
2017-11-06 13:48:34 -0800

    Added notification animation and notification font to the build sytles.css

--- build/components/Powerups/Notification/Notification.js
+++ build/components/Powerups/Notification/Notification.js
@@ -15,17 +15,32 @@ var catchphrases = ['MAIL dominance!', 'MAIL, man!', 'SNAIL MAIL!', 'Going POSTA
 
 var styles = {
   position: 'absolute',
-  top: 0,
+  top: '33%',
   left: 0,
   fontSize: '3vw',
   color: 'black',
-  textShadow: '2px 2px red'
+  textShadow: '2px 2px red',
+  transform: 'scale(0)',
+  transition: '0.5s'
 };
 function CreateNotification() {
   var phrase = catchphrases[0];
   var note = (0, _jquery2.default)('<div class="notification">' + phrase + '</div>');
   note.css(styles);
   (0, _jquery2.default)('.Game').append(note);
+
+  // Animation
+  phrase.css({
+    transform: 'scale(1)'
+  });
+  setTimeout(function () {
+    phrase.css({
+      transform: 'scale(1) translateY(100vh)'
+    });
+    setTimeout(function () {
+      phrase.remove();
+    }, 500);
+  }, 1500);
 }
 
 // Get a random number between two values
