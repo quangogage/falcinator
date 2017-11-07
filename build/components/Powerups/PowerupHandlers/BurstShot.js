d8092df3bf18239b310cb1dbd1897bfe6ae90ebb
Gage Henderson <gage@quangoinc.com>
2017-11-07 09:34:42 -0800

    --allow-empty-message

--- build/components/Powerups/PowerupHandlers/BurstShot.js
+++ build/components/Powerups/PowerupHandlers/BurstShot.js
@@ -17,13 +17,15 @@ function HandleBurstShot(e, ship, world) {
   if (isActive) {
     var shipX = ship.offset().left + ship.width() / 2;
     var shipY = ship.offset().top + ship.height() / 2;
-    var angle = Math.atan2(shipY - e.pageY, shipX - e.pageX) + Math.PI * 1.5;
+    var angle = Math.atan2(shipY - e.pageY, shipX - e.pageX) + Math.PI / 2;
+    angle += Math.pi * 0.1;
     var newX = shipX + Math.cos(angle) * 50;
     var newY = shipY + Math.sin(angle) * 50;
 
     (0, _Bullet.shootBullet)(newX, newY, ship, world);
 
-    angle = Math.atan2(shipY - e.pageY, shipX - e.pageX) + Math.PI * 0.8;
+    angle = Math.atan2(shipY - e.pageY, shipX - e.pageX) + Math.PI / 2;
+    angle -= Math.pi * 0.1;
     newX = shipX + Math.cos(angle) * 50;
     newY = shipY + Math.sin(angle) * 50;
 
