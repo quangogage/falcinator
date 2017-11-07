24453516896386a2ee943a23e7479ae7cb7d4c75
Gage Henderson <gage@quangoinc.com>
2017-11-07 09:59:23 -0800

    --allow-empty-message

--- build/components/Powerups/PowerupHandlers/BouncyBullets.js
+++ build/components/Powerups/PowerupHandlers/BouncyBullets.js
@@ -10,8 +10,8 @@ var _Bullet = require('../../Bullet');
 function BouncyBullets(dt) {
   for (var i = 0; i < _Bullet.bullets.length; i++) {
     var v = _Bullet.bullets[i];
-    if (v.x < 0) {
-      v.x = 0;
+    if (v.x < 10) {
+      v.x = 10;
       v.angle += Math.PI;
     }
   }
