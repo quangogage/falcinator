c70b0107d0137232759b273fc72a2b6fa1bb03ea
Gage Henderson <gage@quangoinc.com>
2017-11-07 10:00:50 -0800

    --allow-empty-message

--- build/components/Powerups/PowerupHandlers/BouncyBullets.js
+++ build/components/Powerups/PowerupHandlers/BouncyBullets.js
@@ -11,8 +11,18 @@ function BouncyBullets(dt) {
   for (var i = 0; i < _Bullet.bullets.length; i++) {
     var v = _Bullet.bullets[i];
     if (v.x < 10) {
-      v.x = 10;
       v.angle += Math.PI;
+      v.x = 10;
+    } else if (v.x > window.innerWidth - 20) {
+      v.angle += Math.PI;
+      v.x = window.innerWidth - 20;
+    }
+    if (v.y < 10) {
+      v.angle += Math.PI;
+      v.y = 10;
+    } else if (v.y > window.innerHeight - 20) {
+      v.angle += Math.PI;
+      v.y = window.innerHeight - 20;
     }
   }
 }
