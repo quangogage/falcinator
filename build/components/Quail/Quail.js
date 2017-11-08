3da02f0580227075e03019521da5e93178cca5f2
Gage Henderson <gage@quangoinc.com>
2017-11-08 15:44:38 -0800

    --allow-empty-message

--- build/components/Quail/Quail.js
+++ build/components/Quail/Quail.js
@@ -75,8 +75,8 @@ function updateQuail(world, bullets, createBlood, createParticle, addScore, subt
         va.setToDelete = true; // Actually gets deleted inside of Bullet.js
         killQuail(i);
         (0, _Camera.ShakeCamera)(5);
-        if (va.isCannonball === true) {
-          (0, _Camera.ShakeCamera)(12);
+        if (va.type === 'cannonball') {
+          (0, _Camera.ShakeCamera)(18);
           var amount = getRandom(7, 12);
           for (var ic = 0; ic < amount; ic++) {
             var x = getRandom(-window.innerWidth, window.innerWidth * 2);
