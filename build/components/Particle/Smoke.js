848b5bcff16beda5d9c2e268288053ad7386a9ad
Gage Henderson <gage@quangoinc.com>
2017-11-09 11:37:12 -0800

    --allow-empty-message

--- build/components/Particle/Smoke.js
+++ build/components/Particle/Smoke.js
@@ -16,17 +16,20 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 var container = [];
 
-// Default styles
-var styles = {
-  position: 'absolute',
-  imageRender: 'pixelated',
-  userSelect: 'none'
-};
-
 // Adjustable variables
 var size = 25; // How big does is it upon creation?
 var colorFadeSpeed = 1; // How quickly does it fade to black?
 var sizeFadeSpeed = 0.75; // How quickly does it shrink?
+var initialColor = [244, 125, 66]; // What color is it upon creation? (RGB)
+
+// Default styles
+var styles = {
+  position: 'absolute',
+  imageRender: 'pixelated',
+  userSelect: 'none',
+  width: size,
+  height: size
+};
 
 var Smoke = function () {
   function Smoke() {
@@ -51,8 +54,13 @@ var Smoke = function () {
         el: el,
         x: x,
         y: y,
-        color: [244, 125, 66]
+        color: initialColor
       };
+
+      // Initial color
+      el.css({
+        background: 'RGB(' + (initialColor[0], initialColor[1], initialColor[2]) + ')'
+      });
 
       // Add to DOM
       (0, _jquery2.default)('.Game').append(el);
