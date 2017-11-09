78ff1aee89aade887de27b02237027b26121e4d5
Gage Henderson <gage@quangoinc.com>
2017-11-09 09:57:47 -0800

    --allow-empty-message

--- build/components/Beam.js
+++ build/components/Beam.js
@@ -96,7 +96,7 @@ function createFlash(x) {
 
   // Basic styles
   flash.css(styles);
-  flash.css({ background: 'blue', left: x, transformOrigin: '50% 50%' });
+  flash.css({ background: 'blue', left: x, transformOrigin: '100%' });
 
   flash.animate({
     width: maxWidth * 2
