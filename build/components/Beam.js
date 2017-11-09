f259c20b531806418822cb447aec2b3ba646c236
Gage Henderson <gage@quangoinc.com>
2017-11-09 09:52:52 -0800

    --allow-empty-message

--- build/components/Beam.js
+++ build/components/Beam.js
@@ -24,10 +24,10 @@ var styles = {
 };
 
 // Adjustable variables
-var growSpeed = 0.5; // How quickly does it's width grow?
+var growSpeed = 0.35; // How quickly does it's width grow?
 var maxWidth = 50; // How wide does it get?
 var sustain = 750; // How long does it stay at full width?
-var shrinkSpeed = 0.05; // How quickly does it shrink after it's sustained full width?
+var shrinkSpeed = 1; // How quickly does it shrink after it's sustained full width?
 
 function shootBeam(mouseX, flags) {
   var el = (0, _jquery2.default)('<div class+="beam"></div>');
