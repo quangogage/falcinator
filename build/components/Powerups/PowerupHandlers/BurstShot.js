9000c14c5e50e65bee9825f0a4bda7abd797f1c1
Gage Henderson <gage@quangoinc.com>
2017-11-08 16:51:14 -0800

    --allow-empty-message

--- build/components/Powerups/PowerupHandlers/BurstShot.js
+++ build/components/Powerups/PowerupHandlers/BurstShot.js
@@ -12,7 +12,7 @@ var _Game = require('../../Game');
 function BurstShot(dt) {
   for (var i = 0; i < _Bullet.bullets.length; i++) {
     var v = _Bullet.bullets[i];
-    if (!v.burstShot && !v.flags.burst && v.timer <= 5) {
+    if (!v.burstShot && !v.flags.burst && v.timer <= 100) {
       var angle1 = v.angle + Math.PI * 0.1;
       var x1 = v.x + Math.cos(angle1) * 10;
       var y1 = v.y + Math.sin(angle1) * 10;
