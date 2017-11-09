4d2b7671cd16b0eef197ba0b403aa0a8de59cc20
Gage Henderson <gage@quangoinc.com>
2017-11-09 11:12:29 -0800

    --allow-empty-message

--- build/components/Bullet/types/Missile.js
+++ build/components/Bullet/types/Missile.js
@@ -8,10 +8,13 @@ var _jquery = require('jquery');
 
 var _jquery2 = _interopRequireDefault(_jquery);
 
+var _Game = require('../../Game');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var img = require('../missile.png');
 
+// Default styles
 var styles = {
   position: 'absolute',
   transformOrigin: '50% 50%',
@@ -21,7 +24,8 @@ var styles = {
 };
 
 // Adjustable Variables
-var speed = 1.25;
+var speed = 0.7;
+var turnSpeed = 0.4;
 
 var Missile = {
   name: 'missile',
@@ -60,19 +64,70 @@ var Missile = {
     v.x += Math.cos(v.angle) * speed * dt;
     v.y += Math.sin(v.angle) * speed * dt;
 
+    // Aiming at the mouse
+    v.angle = aim(v.angle, v.x, v.y, dt);
+
     // Apply to element
-    var newAngle = v.angle + Math.PI / 2;
+    var cssAngle = v.angle + Math.PI / 2;
     v.el.css({
       left: v.x,
       top: v.y,
-      transform: 'rotate(' + newAngle + 'rad)'
+      transform: 'rotate(' + cssAngle + 'rad)'
     });
   }
 };
 
+function aim(angle, x, y, dt) {
+  var target = { x: _Game.mouseX, y: _Game.mouseY };
+  var targetAngleRad = Math.atan2(y - target.y, x - target.x) + Math.PI / 2; // Where the bullet wants to aim
+  var targetAngle = toDegrees(targetAngleRad); // Convert them both to degrees ( from radians )
+  var bulletAngle = toDegrees(angle);
+  if (Math.abs(bulletAngle - targetAngle) <= 5) {
+    // If the angle is basically where it wants to be then do nothing
+    return;
+  }
+  if (Math.abs(bulletAngle - targetAngle) < 180) {
+    // Rotate current directly towards target.
+    if (bulletAngle < targetAngle) {
+      bulletAngle += turnSpeed * dt;
+    } else {
+      bulletAngle -= turnSpeed * dt;
+    }
+  } else {
+    // Rotate the other direction towards target.
+    if (bulletAngle < targetAngle) {
+      bulletAngle -= turnSpeed * dt;
+    } else {
+      bulletAngle += turnSpeed * dt;
+    }
+  }
+  bulletAngle = (bulletAngle % 360 + 360) % 360;
+  return toRadians(bulletAngle);
+}
+
+// ** Helper Functions ** \\
+
 // Get a random number between two values
 function getRandom(min, max) {
   return Math.random() * (max - min + 1) + min;
+}
+
+// Get the distance between two points
+function getDistance(x1, y1, x2, y2) {
+  var a = x1 - x2;
+  var b = y1 - y2;
+
+  return Math.sqrt(a * a + b * b);
+}
+
+// Convert to radians
+function toRadians(degrees) {
+  return degrees * Math.PI / 180;
+}
+
+// Converts from radians to degrees.
+function toDegrees(radians) {
+  return radians * 180 / Math.PI;
 }
 
 exports.default = Missile;
