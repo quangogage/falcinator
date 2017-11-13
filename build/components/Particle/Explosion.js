53ac36dc1b7a8be30db5110f8aa0cf88fb40bc07
Gage Henderson <gage@quangoinc.com>
2017-11-13 13:25:37 -0800

    --allow-empty-message

--- build/components/Particle/Explosion.js
+++ build/components/Particle/Explosion.js
@@ -16,7 +16,9 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 var frames = [require('./images/explosion/1.png'), require('./images/explosion/2.png'), require('./images/explosion/3.png'), require('./images/explosion/4.png'), require('./images/explosion/5.png')];
 
-var framerate = 50;
+// Adjustable variables
+var framerate = 75; // How long is each animation frame?
+var scale = 1.5; // How big is the element?
 
 var Explosion = function () {
   function Explosion() {
@@ -27,30 +29,21 @@ var Explosion = function () {
     key: 'play',
     value: function play(x, y) {
       var frame = 0;
-      var turned = false;
       var div = (0, _jquery2.default)('<img src=' + frames[0] + ' class="explosion" draggable="false" />');
       div.css({
         left: x,
         top: y,
-        transform: 'translateX(-50%) translateY(-50%)'
+        transform: 'translateX(-50%) translateY(-50%)',
+        scale: scale
       });
       (0, _jquery2.default)('.Game').append(div);
       var animation = setInterval(function () {
-        if (turned === false) {
-          if (frame === frames.length - 1) {
-            turned = true;
-          } else {
-            frame += 1;
-          }
-        } else {
-          if (frame !== 0) {
-            frame -= 1;
-          } else {
-            div.remove();
-            clearInterval(animation);
-          }
-        }
+        frame += 1;
         div.attr('src', frames[frame]);
+        if (frame >= frames.length - 1) {
+          div.remove();
+          clearInterval(animation);
+        }
       }, framerate);
     }
   }]);
