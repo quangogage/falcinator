9fbca701e57ff5eec5f48aebf0506a8ae85dff18
Gage Henderson <gage@quangoinc.com>
2017-11-13 17:30:33 -0800

    Added basic Jet file skeleton"

---
+++ build/components/Jet/Jet.js
@@ -0,0 +1,35 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.CreateJet = CreateJet;
+exports.UpdateJet = UpdateJet;
+
+var _jquery = require('jquery');
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var jets = [];
+
+// Images
+var image = {
+  straight: require('./images/straight.png'),
+  up: require('./images/up.png'),
+  down: require('./images/down.png')
+};
+
+// Adjustable variables
+var speed = 0.5; // How fast does the jet go?
+var turnSpeed = 0.25; // How quickly can the jet aim at it's target?
+
+function CreateJet() {}
+
+function UpdateJet(dt) {
+  var i = jets.length;
+  while (i--) {
+    var v = jets[i];
+  }
+}
