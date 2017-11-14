aa5965b8f9735ae7780d55b736d9f57c61045c4c
Gage Henderson <gage@quangoinc.com>
2017-11-14 14:36:35 -0800

    --allow-empty-message

--- build/components/Jet/Attacking.js
+++ build/components/Jet/Attacking.js
@@ -16,6 +16,7 @@ var _HandlePowerups = require('../Powerups/HandlePowerups');
 var burstTime = 500;
 var shootTime = 100;
 var fastShootRate = 50;
+var shootAngleRange = 0.3;
 
 function UpdateAttacking(v, dt) {
   var targetQuail = _Quail.quails[_Quail.quails.length - 1];
@@ -29,31 +30,35 @@ function UpdateAttacking(v, dt) {
   v.target.y = targetQuail.y + targetQuail.el.height() / 2;
 
   //Shooting
-  if (_HandlePowerups.powerupList[0].active) {
-    v.shootTimer += dt;
-    if (v.shootTimer >= fastShootRate) {
-      (0, _Bullet.shootBullet)(v.x + Math.cos(v.angle) * v.el.width() * 0.6, v.y + Math.sin(v.angle) * v.el.width() * 0.6, v.x + Math.cos(v.angle) * v.el.width(), v.y + Math.sin(v.angle) * v.el.width());
-      (0, _Flash.CreateFlash)(v.x + Math.cos(v.angle) * v.el.width(), v.y + Math.sin(v.angle) * v.el.width());
-      v.shootTimer = 0;
-    }
-  } else {
-    v.burstTimer += dt;
-    if (v.burstTimer >= burstTime) {
+  var targetAngle = Math.atan2(v.y - v.target.y, v.x - v.target.x) + Math.PI;
+  var angleDiff = targetAngle - v.angle;
+  if (Math.abs(angleDiff) <= shootAngleRange) {
+    if (_HandlePowerups.powerupList[0].active) {
       v.shootTimer += dt;
-      if (v.shootTimer >= shootTime) {
-        if (v.shotCount < 3) {
-          var orX = v.x + Math.cos(v.angle) * v.el.width() * 0.6;
-          var orY = v.y + Math.sin(v.angle) * v.el.width() * 0.6;
-          var bulletX = orX + Math.cos(-Math.PI / 2) * 10;
-          var bulletY = orY + Math.sin(-Math.PI / 2) * 10;
-          (0, _Bullet.shootBullet)(bulletX, bulletY, bulletX + Math.cos(v.angle) * 10, bulletY + Math.sin(v.angle) * 10);
-          (0, _Flash.CreateFlash)(v.x + Math.cos(v.angle) * v.el.width(), v.y + Math.sin(v.angle) * v.el.width());
-          v.shotCount += 1;
-        } else {
-          v.shotCount = 0;
-          v.burstTimer = 0;
-        }
+      if (v.shootTimer >= fastShootRate) {
+        (0, _Bullet.shootBullet)(v.x + Math.cos(v.angle) * v.el.width() * 0.6, v.y + Math.sin(v.angle) * v.el.width() * 0.6, v.x + Math.cos(v.angle) * v.el.width(), v.y + Math.sin(v.angle) * v.el.width());
+        (0, _Flash.CreateFlash)(v.x + Math.cos(v.angle) * v.el.width(), v.y + Math.sin(v.angle) * v.el.width());
         v.shootTimer = 0;
+      }
+    } else {
+      v.burstTimer += dt;
+      if (v.burstTimer >= burstTime) {
+        v.shootTimer += dt;
+        if (v.shootTimer >= shootTime) {
+          if (v.shotCount < 3) {
+            var orX = v.x + Math.cos(v.angle) * v.el.width() * 0.6;
+            var orY = v.y + Math.sin(v.angle) * v.el.width() * 0.6;
+            var bulletX = orX + Math.cos(-Math.PI / 2) * 10;
+            var bulletY = orY + Math.sin(-Math.PI / 2) * 10;
+            (0, _Bullet.shootBullet)(bulletX, bulletY, bulletX + Math.cos(v.angle) * 10, bulletY + Math.sin(v.angle) * 10);
+            (0, _Flash.CreateFlash)(v.x + Math.cos(v.angle) * v.el.width(), v.y + Math.sin(v.angle) * v.el.width());
+            v.shotCount += 1;
+          } else {
+            v.shotCount = 0;
+            v.burstTimer = 0;
+          }
+          v.shootTimer = 0;
+        }
       }
     }
   }
