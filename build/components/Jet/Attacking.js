b1f66872f6bbb2851061001d97565401914c5732
Gage Henderson <gage@quangoinc.com>
2017-11-14 11:13:32 -0800

    --allow-empty-message

--- build/components/Jet/Attacking.js
+++ build/components/Jet/Attacking.js
@@ -8,8 +8,8 @@ var changeTimeRange = [750, 1250];
 var changeTime = getRandom(changeTimeRange[0], changeTimeRange[1]);
 
 function UpdateAttacking(v, dt) {
-  console.log("updating attacking status");
   v.timer += dt;
+  console.log(v.timer);
   if (v.timer >= changeTime) {
     v.target.x = getRandom(0, window.innerWidth);
     v.target.y = getRandom(0, window.innerHeight);
