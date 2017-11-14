864aabf12142a8e69d324d94356bc05ff25b7b18
Gage Henderson <gage@quangoinc.com>
2017-11-14 14:28:47 -0800

    --allow-empty-message

--- build/components/Jet/Attacking.js
+++ build/components/Jet/Attacking.js
@@ -11,8 +11,11 @@ var _Bullet = require('../Bullet/Bullet');
 
 var _Flash = require('../Flash');
 
+var _HandlePowerups = require('../Powerups/HandlePowerups');
+
 var burstTime = 500;
 var shootTime = 100;
+var fastShootRate = 50;
 
 function UpdateAttacking(v, dt) {
   var targetQuail = _Quail.quails[_Quail.quails.length - 1];
@@ -26,19 +29,28 @@ function UpdateAttacking(v, dt) {
   v.target.y = targetQuail.y + targetQuail.el.height() / 2;
 
   //Shooting
-  v.burstTimer += dt;
-  if (v.burstTimer >= burstTime) {
+  if (_HandlePowerups.powerupList[0].active) {
     v.shootTimer += dt;
-    if (v.shootTimer >= shootTime) {
-      if (v.shotCount < 3) {
-        (0, _Bullet.shootBullet)(v.x + Math.cos(v.angle) * v.el.width() * 0.6, v.y + Math.sin(v.angle) * v.el.width() * 0.6, v.x + Math.cos(v.angle) * v.el.width(), v.y + Math.sin(v.angle) * v.el.width());
-        (0, _Flash.CreateFlash)(v.x + Math.cos(v.angle) * v.el.width(), v.y + Math.sin(v.angle) * v.el.width());
-        v.shotCount += 1;
-      } else {
-        v.shotCount = 0;
-        v.burstTimer = 0;
-      }
+    if (v.shootTimer >= fastShootRate) {
+      (0, _Bullet.shootBullet)(v.x + Math.cos(v.angle) * v.el.width() * 0.6, v.y + Math.sin(v.angle) * v.el.width() * 0.6, v.x + Math.cos(v.angle) * v.el.width(), v.y + Math.sin(v.angle) * v.el.width());
+      (0, _Flash.CreateFlash)(v.x + Math.cos(v.angle) * v.el.width(), v.y + Math.sin(v.angle) * v.el.width());
       v.shootTimer = 0;
+    }
+  } else {
+    v.burstTimer += dt;
+    if (v.burstTimer >= burstTime) {
+      v.shootTimer += dt;
+      if (v.shootTimer >= shootTime) {
+        if (v.shotCount < 3) {
+          (0, _Bullet.shootBullet)(v.x + Math.cos(v.angle) * v.el.width() * 0.6, v.y + Math.sin(v.angle) * v.el.width() * 0.6, v.x + Math.cos(v.angle) * v.el.width(), v.y + Math.sin(v.angle) * v.el.width());
+          (0, _Flash.CreateFlash)(v.x + Math.cos(v.angle) * v.el.width(), v.y + Math.sin(v.angle) * v.el.width());
+          v.shotCount += 1;
+        } else {
+          v.shotCount = 0;
+          v.burstTimer = 0;
+        }
+        v.shootTimer = 0;
+      }
     }
   }
 }
