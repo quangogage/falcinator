e95271440aecb905a80c4fcfa1903dd7f56af350
Gage Henderson <gage@quangoinc.com>
2017-11-15 10:25:04 -0800

    --allow-empty-message

--- build/components/WeaponHandler.js
+++ build/components/WeaponHandler.js
@@ -9,6 +9,10 @@ var _Bullet = require('./Bullet/Bullet');
 
 var _Game = require('./Game');
 
+var _Lose = require('./Lose');
+
 function WeaponHandler(mouseX, mouseY) {
-  (0, _Bullet.shootBullet)(_Game.ship.offset().left + _Game.ship.width() / 2, _Game.ship.offset().top + _Game.ship.height() / 2, mouseX, mouseY);
+  if (!_Lose.hasLost) {
+    (0, _Bullet.shootBullet)(_Game.ship.offset().left + _Game.ship.width() / 2, _Game.ship.offset().top + _Game.ship.height() / 2, mouseX, mouseY);
+  }
 }
