15010000aab93ebdf1afec0aa53e38585fcca931
Gage Henderson <gage@quangoinc.com>
2017-11-14 16:02:42 -0800

    --allow-empty-message

--- build/components/Timer/Timer.js
+++ build/components/Timer/Timer.js
@@ -25,18 +25,18 @@ var styles = {
 
 function LoadTimer() {
   timer = 0;
-  el = (0, _jquery2.default)('<div class="timer">' + timer.toHHMMSS() + '</div>');
+  el = (0, _jquery2.default)('<div class="timer">' + toHHMMSS(timer) + '</div>');
   el.css(styles);
   (0, _jquery2.default)('.Game').append(el);
 }
 function UpdateTimer(dt) {
   timer -= dt;
-  el.html(timer.toHHMMSS());
+  el.html(toHHMMSS(timer));
 }
 
 // Format string to HHMMSS
-String.prototype.toHHMMSS = function () {
-  var sec_num = parseInt(this, 10); // don't forget the second param
+function toHHMMSS(time) {
+  var sec_num = parseInt(time, 10); // don't forget the second param
   var hours = Math.floor(sec_num / 3600);
   var minutes = Math.floor((sec_num - hours * 3600) / 60);
   var seconds = sec_num - hours * 3600 - minutes * 60;
@@ -52,4 +52,4 @@ String.prototype.toHHMMSS = function () {
     seconds = '0' + seconds;
   }
   return minutes + ':' + seconds + ':' + milli;
-};
+}
