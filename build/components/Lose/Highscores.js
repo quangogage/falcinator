3ed44069cfccdfa5850fb639c934f111b598c826
Gage Henderson <gage@quangoinc.com>
2017-11-20 13:45:57 -0800

    --allow-empty-message

--- build/components/Lose/Highscores.js
+++ build/components/Lose/Highscores.js
@@ -21,7 +21,18 @@ var config = {
   messagingSenderId: '842435348000'
 };
 _firebase2.default.initializeApp(config);
+var database = _firebase2.default.database();
+
+// Triggered once when you lose
 function HandleHighscore() {
   // Submit Dummy Score
-  _firebase2.default.push({ name: 'test score', score: '1250890' });
+  addHighScore('testing');
+}
+
+// Add a high score
+function addHighScore(name) {
+  database.ref('/scores' + name).set({
+    name: name,
+    score: '150293'
+  });
 }
