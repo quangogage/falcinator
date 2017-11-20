16cbfeb2a596cfbde6887efe0ad05790a29eb859
Gage Henderson <gage@quangoinc.com>
2017-11-20 13:40:58 -0800

    --allow-empty-message

--- build/components/Lose/Highscores.js
+++ build/components/Lose/Highscores.js
@@ -12,17 +12,16 @@ var _firebase2 = _interopRequireDefault(_firebase);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // Executed on lose
+var config = {
+  apiKey: 'AIzaSyBtu5nBaAeIItHuhHRVfp6mXiL9dLpqyLA',
+  authDomain: 'falcinator-988f4.firebaseapp.com',
+  databaseURL: 'https://falcinator-988f4.firebaseio.com',
+  projectId: 'falcinator-988f4',
+  storageBucket: 'falcinator-988f4.appspot.com',
+  messagingSenderId: '842435348000'
+};
+_firebase2.default.initializeApp(config);
 function HandleHighscore() {
-  var config = {
-    apiKey: 'AIzaSyBtu5nBaAeIItHuhHRVfp6mXiL9dLpqyLA',
-    authDomain: 'falcinator-988f4.firebaseapp.com',
-    databaseURL: 'https://falcinator-988f4.firebaseio.com',
-    projectId: 'falcinator-988f4',
-    storageBucket: 'falcinator-988f4.appspot.com',
-    messagingSenderId: '842435348000'
-  };
-  _firebase2.default.initializeApp(config);
-
   // Submit Dummy Score
   _firebase2.default.push({ name: 'test score', score: '1250890' });
 }
