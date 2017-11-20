58d28ae9800f6e824179da42822dac1e639f3b54
Gage Henderson <gage@quangoinc.com>
2017-11-20 10:21:43 -0800

    --allow-empty-message

--- build/components/Game.js
+++ build/components/Game.js
@@ -151,6 +151,7 @@ var Game = function (_React$Component) {
   }, {
     key: 'render',
     value: function render() {
+      console.log(_Lose.hasLost);
       var RenderLosePrompt = _Lose.hasLost ? _react2.default.createElement(_LosePrompt2.default, null) : _react2.default.createElement('div', null);
       return _react2.default.createElement(
         'div',
