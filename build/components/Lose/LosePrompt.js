27d827996626060aa37bfaf2f863ca2441e26d28
Gage Henderson <gage@quangoinc.com>
2017-11-22 11:24:07 -0800

    --allow-empty-message

--- build/components/Lose/LosePrompt.js
+++ build/components/Lose/LosePrompt.js
@@ -24,6 +24,8 @@ var _jquery = require('jquery');
 
 var _jquery2 = _interopRequireDefault(_jquery);
 
+var _Quail = require('../Quail/Quail');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
