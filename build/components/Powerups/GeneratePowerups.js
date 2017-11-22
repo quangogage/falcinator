7f800238f552e76d3102b9f237150ba3e69d8972
Gage Henderson <gage@quangoinc.com>
2017-11-22 10:39:03 -0800

    --allow-empty-message

--- build/components/Powerups/GeneratePowerups.js
+++ build/components/Powerups/GeneratePowerups.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = GeneratePowerups;
 var timer = 0;
 var spawnRate = 100; // How often should it roll for a powerup?
-var spawnChanceRange = 2; // How likely is it that a powerup spawns ( percentage ) ?
+var spawnChanceRange = 3.2; // How likely is it that a powerup spawns ( percentage ) ?
 var waitTimer = 0;
 var waitTime = 20000; // How long into playing until powerups start spawning?
 /*spawnChanceRange = 100;
